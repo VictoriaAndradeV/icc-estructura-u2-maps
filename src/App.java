14cd1ce1fec0be1f273dcf5f375c439d91d80fd5
Victoria Andrade <vandradev2@est.ups.edu.ec>
2025-07-08 08:37:51 -0500

    avance clase 08/07/2025

--- src/App.java
+++ src/App.java
@@ -1,8 +1,4 @@
-import controllers.Ejercicios;
-import controllers.EmpleadoContoller;
 import controllers.Mapa;
-
-import models.Empleado;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -22,7 +18,10 @@ public class App {
     }
 
     private static void runMapExamlpe() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        Mapa mapa = new Mapa();
+        mapa.ejemploConHashMap();
+        mapa.ejemploConLinkedHashMap();
+        mapa.ejemploConTreeMap();
     }
 
     private static void runEjerccios() {
--- src/controllers/Mapa.java
+++ src/controllers/Mapa.java
@@ -1,8 +1,84 @@
 package controllers;
-
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import models.Empleado;
 
 public class Mapa {
+
     public Mapa() {
+    }
+
+    public void ejemploConHashMap(){
+        Map<String, String> capitales = new HashMap<>();
+        capitales.put("Ecuador", "Quito");
+        capitales.put("Peru", "Lima");
+        capitales.put("Colombia", "Bogota");
+
+        System.out.println("Mapa de capitales");
+        for(String pais : capitales.keySet()){
+            System.out.println("Pais: " + pais + ", Capital: " + capitales);
+        }
+
+        //ejemplo con acceso directo
+        String capital = capitales.get("Ecuador");
+        System.out.println("La capital de Ecuador es: " + capital);
+
+        System.out.println("\n");
+
+        Map<Integer, Empleado> empleados = new HashMap<>();
+        empleados.put(3, new Empleado(3, "Pedro", "Tester"));
+        empleados.put(1, new Empleado(1, "Ana", "Dev"));
+        empleados.put(2, new Empleado(2, "Luis", "Diseñador"));
+        empleados.put(1, new Empleado(3, "Pedro", "Tester"));
+        
+        for(Map.Entry<Integer, Empleado> entry : empleados.entrySet()){
+        System.out.println("Clave: "   + entry.getKey() + ", Valor: "   + entry.getValue() + ", Posicion: "+ entry.getKey());
+        }
+
+        Map<Empleado, Integer> empleadoDos = new HashMap<>();
+        Empleado emp1 = new Empleado(1, "Ana", "Dev");//aunque tengamos dos anas repetidos, se imprimen
+        Empleado emp2 = new Empleado(2, "Pedro", "Dev");//porq tienen nombre de variables diferentes 
+        Empleado emp3 = new Empleado(3, "Ana", "Dev");
+        Empleado emp4 = new Empleado(3, "Ana", "Dev");
+
+
+        empleadoDos.put(emp1, 1000);
+        empleadoDos.put(emp2, 1200);
+        empleadoDos.put(emp3, 1500);
+        empleadoDos.put(emp4, 2000);
+
+        for(Map.Entry<Empleado, Integer> entry : empleadoDos.entrySet()){
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        }
+
+    }
+
+    public void ejemploConLinkedHashMap(){
+        LinkedHashMap<String, Integer> producto = new LinkedHashMap<>();
+        producto.put("Leche", 5);
+        producto.put("Caja de fresas", 7);
+        producto.put("Lata de Aceitunas", 3);
+        producto.put("Chocolate", 4);
+        producto.put("Mani", 2);
+
+        System.out.println("\nMapa de productos");
+        for(String carrito: producto.keySet()){
+            System.out.println("Producto = " + carrito + ", Cantidad = " + producto.get(carrito));
+        }
+    }
+
+    public void ejemploConTreeMap(){
+        LinkedHashMap<Integer, String> persona = new LinkedHashMap<>();
+        persona.put(123, "Dayana");
+        persona.put(456, "Mateo");
+        persona.put(789, "Martin");
+        persona.put(159, "Erika");
+        persona.put(753, "Lorena");
+
+        System.out.println("\nLista de personas:");
+        for (Map.Entry<Integer, String> entry : persona.entrySet()) {
+            System.out.println("Cédula = " + entry.getKey() + ", Nombre = " + entry.getValue());
+        }
     }
 }
